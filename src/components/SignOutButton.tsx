b878de37248cba0a392bf3c311e702126c34cba9
Sri2103 <damasriharsha@gmail.com>
2023-04-12 20:15:09 -0400

    basic authentication complete

---
+++ src/components/SignOutButton.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import React, { ButtonHTMLAttributes, FC, useState } from 'react'
+import Button from './ui/Button'
+import { signOut } from 'next-auth/react'
+import { toast } from 'react-hot-toast'
+import { Loader2, LogOut } from 'lucide-react'
+
+interface SignOutButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const SignOutButton: FC<SignOutButtonProps> = ({...props}) => {
+    const [isSignOut, setIsSignOut] =useState<boolean>(false)
+  return (
+    <Button {...props} variant="ghost" onClick={async()=>{
+        setIsSignOut(true)
+        try{
+            await signOut()
+        }catch (error){
+            toast.error('there was a problem signout ')
+        }finally{
+            setIsSignOut(false)
+        }
+    }}>
+
+        {isSignOut? (<Loader2 className='animate-spin h-4 w-4' />):(<LogOut className='h-4 w-4' />)}
+    </Button>
+  )
+}
+
+export default SignOutButton
