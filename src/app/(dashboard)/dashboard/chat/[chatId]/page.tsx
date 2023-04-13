3cc7e99a3a8af13cbe9771912042b1742aeb0a06
Sri2103 <damasriharsha@gmail.com>
2023-04-13 17:14:05 -0400

    chat and message validations

---
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -0,0 +1,48 @@
+import { fetchRedis } from '@/helpers/redis'
+import { db } from '@/lib/db'
+import { messageArrayValidator } from '@/lib/validations/messages'
+import { getSession } from 'next-auth/react'
+import { notFound } from 'next/navigation'
+import React, { FC } from 'react'
+interface pageprops{
+    params:{
+        chatId: string
+    }
+}
+
+const getChatMessages = async(chatId: string) =>{
+    try {
+        const results:string[] = await fetchRedis('zrange',`chat:${chatId}:messages`, 0, -1)
+        const dbMessages = results.map((message)=>{
+            return JSON.parse(message) as Message
+        })
+        const reversedDBmessages = dbMessages.reverse()
+        const messages = messageArrayValidator.parse(reversedDBmessages)
+        return messages
+    } catch (error) {
+        notFound()
+        
+    }
+}
+
+const page = async ({params}:pageprops) => {
+    const {chatId} = params
+    const session = await getSession()
+    if(!session) notFound()
+    const {user} = session
+    const [userId1, userId2] = chatId.split("--")
+    if (user.id !== userId1 && user.id !== userId2){
+        notFound()
+    }
+
+    const chatPartnerId = user.id === userId1 ? userId2 : userId1
+    const chatPartner = (await db.get(`user:${chatPartnerId}`)) as User
+
+    const initialMessage = await getChatMessages(chatId)
+
+  return (
+    <div>{params.chatId}</div>
+  )
+}
+
+export default page
